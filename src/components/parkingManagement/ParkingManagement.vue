908be7d469497b1e92faa2ed19bfffadc1b6e97d
wangtx <wang.tianxia@foxmail.com>
2020-12-23 21:56:18 +0800

    update

---
+++ src/components/parkingManagement/ParkingManagement.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h1>Hello Vue.js! This is intended to be the ParkingManagement page.</h1>
+    <h2>这是车库端管理页面</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ParkingManagement'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import VueRouter from 'vue-router'
+
+import ParkingManagement from './components/parkingManagement/ParkingManagement.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(Antd)
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/parkingManagement', component: ParkingManagement },
+  { path: '/helloWorld', component: HelloWorld}
+]
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
